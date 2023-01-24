a5e07a8cbcd0f1a3a9c5904cd3e0ad95e66f184f
Jimena <jimenanrp2@gmail.com>
2023-01-24 17:55:11 +0100

    add function and main

--- strictEquals.js
+++ strictEquals.js
@@ -1,3 +1,11 @@
-const strictEquals = (x, y) => {
-
-}
+export const strictEquals = (x, y) => {
+  if (Object.is(x, NaN) && Object.is(y, NaN)) {
+    return false;
+  } else if (Object.is(x, 0) && Object.is(y, -0)) {
+    return true;
+  } else if (Object.is(x, -0) && Object.is(y, 0)) {
+    return true;
+  } else {
+    return Object.is(x, y);
+  }
+};
