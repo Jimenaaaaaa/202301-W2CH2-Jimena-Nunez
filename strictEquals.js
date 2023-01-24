f9115d283695f7d4cb9c08a416a6afbbb9c06809
Jimena <jimenanrp2@gmail.com>
2023-01-24 17:57:12 +0100

    fix eslint suggestions

--- strictEquals.js
+++ strictEquals.js
@@ -1,11 +1,15 @@
 export const strictEquals = (x, y) => {
   if (Object.is(x, NaN) && Object.is(y, NaN)) {
     return false;
-  } else if (Object.is(x, 0) && Object.is(y, -0)) {
-    return true;
-  } else if (Object.is(x, -0) && Object.is(y, 0)) {
-    return true;
-  } else {
-    return Object.is(x, y);
   }
+
+  if (Object.is(x, 0) && Object.is(y, -0)) {
+    return true;
+  }
+
+  if (Object.is(x, -0) && Object.is(y, 0)) {
+    return true;
+  }
+
+  return Object.is(x, y);
 };
